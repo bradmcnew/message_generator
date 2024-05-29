63f3e8d944dae8afd7d712dbec5556bbc2cb394a
Brad McNew <mcnewbrad@gmail.com>
2024-05-29 17:20:22 -0400

    WIP: work on player objects and events

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+const players = [
+    //C's
+    {name: 'Jayson Tatum', team: 'Celtics', skillLevel: 95},
+    {name: 'Jaylen Brown', team: 'Celtics', skillLevel: 91},
+    {name: 'Kristaps Porzingis', team: 'Celtics', skillLevel: 88},
+    {name: 'Jrue Holiday', team: 'Celtics', skillLevel: 87},
+    {name: 'Derrick White', team: 'Celtics', skillLevel: 87},
+    //Mav's
+    {name: 'Luka Doncic', team: 'Mavs', skillLevel: 95},
+    {name: 'Kyrie Irving', team: 'Mavs', skillLevel: 93},
+    {name: 'Derrick Lively', team: 'Mavs', skillLevel: 82},
+    {name: 'Daniel Gafford', team: 'Mavs', skillLevel: 82},
+    {name: 'P.J. Washington', team: 'Mavs', skillLevel: 80},
+]
+
+const goodEvents = ['hits a game winner', `scores ${Math.floor(Math.random()* 20 + 40)}`];
+const badEvents = [];
