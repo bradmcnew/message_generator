59951bb45ca90de8d0c619192045b484ec8fdc57
Brad McNew <mcnewbrad@gmail.com>
2024-05-29 21:40:56 -0400

    Finish README and format

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# git_practice
+# <Your-Project-Title>
+
+## Description
+
+I'm a HUGE Celtics fan and I was prompted to make a random message generator so I decided to do one on the 2024 NBA Finals.
+This project is a fun (but likely innacurate) prediction model of the 2024 NBA Finals.
+I learned how to implement my JavaScript skills, Markdown skills, and how to use version control with git and Github.
+
+## Installation
+
+Ensure you have the following installed on your machine:
+
+- [Node.js](https://nodejs.org/) (which includes npm)
+
+### Steps
+
+1. **Clone the repository**:
+   ```sh
+   git clone https://github.com/bradmcnew/message_generator.git
+
+## Usage
+
+To use, simply enter node script.js and the simulator will be run.
+
+## Credits
+
+I used NBA2k24 Player Ratings: https://www.2kratings.com/teams/ to help with my simulation.
+
+## License
+
+MIT License
+
+Copyright (c) 2024 Bradford Isaac McNew
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- script.js
+++ script.js
@@ -2,18 +2,26 @@
 
 const players = [
     //C's
-    {name: 'Jayson Tatum', team: 'Celtics', skillLevel: 95},
-    {name: 'Jaylen Brown', team: 'Celtics', skillLevel: 91},
-    {name: 'Kristaps Porzingis', team: 'Celtics', skillLevel: 88},
-    {name: 'Jrue Holiday', team: 'Celtics', skillLevel: 87},
-    {name: 'Derrick White', team: 'Celtics', skillLevel: 87},
+    { name: 'Jayson Tatum', team: 'Celtics', skillLevel: 95 },
+    { name: 'Jaylen Brown', team: 'Celtics', skillLevel: 91 },
+    { name: 'Kristaps Porzingis', team: 'Celtics', skillLevel: 88 },
+    { name: 'Jrue Holiday', team: 'Celtics', skillLevel: 87 },
+    { name: 'Derrick White', team: 'Celtics', skillLevel: 87 },
     //Mav's
-    {name: 'Luka Doncic', team: 'Mavs', skillLevel: 97},
-    {name: 'Kyrie Irving', team: 'Mavs', skillLevel: 93},
-    {name: 'Derrick Lively', team: 'Mavs', skillLevel: 82},
-    {name: 'Daniel Gafford', team: 'Mavs', skillLevel: 82},
-    {name: 'P.J. Washington', team: 'Mavs', skillLevel: 80},
+    { name: 'Luka Doncic', team: 'Mavs', skillLevel: 97 },
+    { name: 'Kyrie Irving', team: 'Mavs', skillLevel: 93 },
+    { name: 'Derrick Lively', team: 'Mavs', skillLevel: 82 },
+    { name: 'Daniel Gafford', team: 'Mavs', skillLevel: 82 },
+    { name: 'P.J. Washington', team: 'Mavs', skillLevel: 80 },
 ];
+
+const goodEvents = ['hit a HUGE game winner', `scored ${Math.floor(Math.random() * 20 + 40)} points`,
+    'had a CLUTCH game saving block', 'led the blowout',
+    `commands the ${Math.floor(Math.random() * 20 + 1)} point victory`, `hit 10 three-pointers`];
+const badEvents = ['fouled out late in the fourth', `shot ${Math.floor(Math.random() * 20 + 15)}% from the floor`,
+    `had ${Math.floor(Math.random() * 5 + 5)} turnovers`, 'had 0 points'];
+
+
 //calculates skill total of team
 const teamSkill = arr => {
     let skillCount = 0;
@@ -23,38 +31,35 @@ const teamSkill = arr => {
     return skillCount;
 }
 
-const goodEvents = ['hit a HUGE game winner', `scored ${Math.floor(Math.random()* 20 + 40)} points`,
-                    'had a CLUTCH game saving block', 'led the blowout', 
-                    `commands the ${Math.floor(Math.random()* 20 + 1)} point victory`,`hit 10 three-pointers`];
-const badEvents = ['fouled out late in the fourth', `shot ${Math.floor(Math.random()* 20 + 15)}% from the floor`,
-                    `had ${Math.floor(Math.random()* 5 + 5)} turnovers`, 'had 0 points'];
+
 
 //celtics players array
-const celticsPlayers = players.filter(player => {return player.team === 'Celtics'});
+const celticsPlayers = players.filter(player => { return player.team === 'Celtics' });
 //mavs players array
-const mavsPlayers = players.filter(player => {return player.team === 'Mavs'});
-//celtics skill
+const mavsPlayers = players.filter(player => { return player.team === 'Mavs' });
+
+//team skill arrays
 const celticsSkill = teamSkill(celticsPlayers);
 const mavsSkill = teamSkill(mavsPlayers);
 const totalSkill = celticsSkill + mavsSkill;
 
 //game simulation
 const simGame = () => {
-  if (Math.floor(Math.random() * totalSkill + 1) >= 448) {
-    //mavs win
-    return 'm';
-  }
-  //celtics win
-  return 'c';
+    if (Math.floor(Math.random() * totalSkill + 1) >= 448) {
+        //mavs win
+        return 'm';
+    }
+    //celtics win
+    return 'c';
 }
 
-const celticsNames = celticsPlayers.map(player => {return player.name});
-const mavsNames = mavsPlayers.map(player => {return player.name});
+const celticsNames = celticsPlayers.map(player => { return player.name });
+const mavsNames = mavsPlayers.map(player => { return player.name });
 const eventSelector = (team) => {
     if (team === 'mavs') {
         if (Math.floor(Math.random() * 2 < 1)) {
             return (`${mavsNames[Math.floor(Math.random() * mavsNames.length)]} ` +
-            `${goodEvents[Math.floor(Math.random() * goodEvents.length)]} to WIN game`);
+                `${goodEvents[Math.floor(Math.random() * goodEvents.length)]} to WIN game`);
         }
         return (`${celticsNames[Math.floor(Math.random() * celticsNames.length)]} ` +
             `${badEvents[Math.floor(Math.random() * badEvents.length)]} and LOST game`);
@@ -62,7 +67,7 @@ const eventSelector = (team) => {
     else {
         if (Math.floor(Math.random() * 2 < 1)) {
             return (`${celticsNames[Math.floor(Math.random() * celticsNames.length)]} ` +
-            `${goodEvents[Math.floor(Math.random() * goodEvents.length)]} to WIN game`);
+                `${goodEvents[Math.floor(Math.random() * goodEvents.length)]} to WIN game`);
         }
         return (`${mavsNames[Math.floor(Math.random() * mavsNames.length)]} ` +
             `${badEvents[Math.floor(Math.random() * badEvents.length)]} and LOST game`);
@@ -92,7 +97,7 @@ let bestOfSeven = () => {
                 console.log(`\nThe Dallas Mavericks win the NBA Finals!`)
             }
             else {
-                console.log(seriesScore(mavsWins, celticsWins)+'\n');
+                console.log(seriesScore(mavsWins, celticsWins) + '\n');
             }
         }
         else {
@@ -102,7 +107,7 @@ let bestOfSeven = () => {
                 console.log(`\nThe Boston Celtics win the NBA Finals!`)
             }
             else {
-                console.log(seriesScore(mavsWins, celticsWins)+'\n');
+                console.log(seriesScore(mavsWins, celticsWins) + '\n');
             }
         }
     }
